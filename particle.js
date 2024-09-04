314d364cccb915ec796b2250cf8fc2b8f9fbe60c
TeamKid <shenoybaroi@gmail.com>
2024-09-04 13:50:34 +1000

    Added base contacts section at the bottom

---
+++ particle.js
@@ -0,0 +1,11 @@
+reveals = document.querySelectorAll(".reveal");
+
+document.addEventListener('scroll', (scroll)=>{
+    const revealHeight = 50;
+
+    for(let i = 0; i < reveals.length; ++i){
+        if(window.innerHeight - reveals[i].getBoundingClientRect().top > revealHeight){
+            reveals[i].classList.add("active");
+        } 
+    }
+});
